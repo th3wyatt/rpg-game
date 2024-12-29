3046fd4df6cef76c06241c61176f1a39aed4cb4a
Jon <jonathan.k.wyatt@gmail.com>
2024-12-29 12:20:32 -0800

    added a combo timer to reset combo after a short time and sped up attack animation

--- Scripts/Characters/Player/PlayerAttackState.cs
+++ Scripts/Characters/Player/PlayerAttackState.cs
@@ -5,11 +5,19 @@ public partial class PlayerAttackState : PlayerState
 {
     private int comboCounter = 1;
     private int comboCounterMax = 2;
+    [Export] private Timer comboTimerNode;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        //lamda to execute a simple action rather than setup a standard method
+        comboTimerNode.Timeout += () => comboCounter = 1; 
+    }
 
     protected override void EnterState()
     {
         CharacterNode.AnimationPlayerNode.Play(
-            GameConstants.ANIM_ATTACK + comboCounter
+            GameConstants.ANIM_ATTACK + comboCounter, -1, 1.5f
         );
 
         CharacterNode.AnimationPlayerNode.AnimationFinished += HandleAnimationFinished;
@@ -18,6 +26,7 @@ public partial class PlayerAttackState : PlayerState
     protected override void ExitState()
     {
         CharacterNode.AnimationPlayerNode.AnimationFinished -= HandleAnimationFinished;
+        comboTimerNode.Start();
     }
 
     private void HandleAnimationFinished(StringName animName)
