a6387f1ef23a85bf09221dafe573031672e7580c
Jon <jonathan.k.wyatt@gmail.com>
2024-12-31 19:24:16 -0800

    added hurtbox for enemy and hitbox for player

--- Scripts/Characters/Character.cs
+++ Scripts/Characters/Character.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 public abstract partial class Character : CharacterBody3D
@@ -12,9 +13,20 @@ public abstract partial class Character : CharacterBody3D
     [Export] public NavigationAgent3D AgentNode { get; private set;}
     [Export] public Area3D ChaseAreaNode { get; private set;}
     [Export] public Area3D AttackAreaNode { get; private set;}
+    [Export] public Area3D HurtboxNode { get; private set;}
     public Vector2 direction = new();
 
-    
+    public override void _Ready()
+    {
+        base._Ready();
+        HurtboxNode.AreaEntered += HandleHurtboxAreaEntered;
+    }
+
+    private void HandleHurtboxAreaEntered(Area3D area)
+    {
+        GD.Print("Shape Detected: " + area.GetParent().Name);
+    }
+
     public void Flip()
     {
         bool isNotMovingHorizontally = Velocity.X == 0;
